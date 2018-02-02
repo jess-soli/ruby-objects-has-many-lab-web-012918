629e9c51979cdf1698951142bfe182f94883890b
Jessica Solimano <jessica.solimano@flatironschool.com>
2018-02-02 21:12:00 +0000

    automatically backed up by learn

--- lib/run.rb
+++ lib/run.rb
@@ -7,6 +7,8 @@ require 'pry'
 adele = Artist.new("Adele")
 beyonce = Artist.new("Beyonce")
 
+say_my_name = Song.New("Say My Name", "Beyonce")
+
 # tweet1 = coffee_dad.post_tweet('great cofee')
 # tweet2 = coffee_dad.post_tweet('havin coffee')
 # tweet3 = tea_uncle.post_tweet('havin tea')
