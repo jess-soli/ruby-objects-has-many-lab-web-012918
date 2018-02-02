34faec10a6d555785c1fe4433a164fb48a41ee0b
Jessica Solimano <jessica.solimano@flatironschool.com>
2018-02-02 20:32:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,9 +7,12 @@ class Artist
     @name = name
   end
   
+  def add_song_by_name(name)
+    song = song.New(name, self) 
+  end
+  
   def songs
     Song.all.select { |song| song.artist == self }
-    end
   end
   
 end
