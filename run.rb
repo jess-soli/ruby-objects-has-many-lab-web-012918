d6d76d6ff729a7b597184cda6eda62b3e7efd51a
Jessica Solimano <jessica.solimano@flatironschool.com>
2018-02-02 20:17:52 +0000

    automatically backed up by learn

--- run.rb
+++ run.rb
@@ -4,17 +4,17 @@ require_relative './author'
 require_relative './post'
 require 'pry'
 
-coffee_dad = User.new('coffee_dad')
-tea_uncle = User.new('tea_uncle')
+adele = Artist.new("Adele")
+beyonce = Artist.new("Beyonce")
 
-tweet1 = coffee_dad.post_tweet('great cofee')
-tweet2 = coffee_dad.post_tweet('havin coffee')
-tweet3 = tea_uncle.post_tweet('havin tea')
-tweet4 = coffee_dad.post_tweet('havin coffee')
-tweet5 = tea_uncle.post_tweet('i like tea')
+# tweet1 = coffee_dad.post_tweet('great cofee')
+# tweet2 = coffee_dad.post_tweet('havin coffee')
+# tweet3 = tea_uncle.post_tweet('havin tea')
+# tweet4 = coffee_dad.post_tweet('havin coffee')
+# tweet5 = tea_uncle.post_tweet('i like tea')
 
-coffee_dad.tweets
+# coffee_dad.tweets
 
 binding.pry
 
-'k bye'
+'calling'
