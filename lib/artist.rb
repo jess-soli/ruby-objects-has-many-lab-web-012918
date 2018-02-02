13b7aca073d7a2ee3f3c70ccd2bf22838e209265
Jessica Solimano <jessica.solimano@flatironschool.com>
2018-02-02 20:31:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,12 +5,10 @@ class Artist
   
   def initialize(name) 
     @name = name
-    @songs = []
   end
   
   def songs
-    Song.all.select do |song|
-      song.user == self
+    Song.all.select { |song| song.artist == self }
     end
   end
   
